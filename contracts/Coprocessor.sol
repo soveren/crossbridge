eed88b6f1e9b8530eff77bc9533af7e33ea82ec1
Slavik Bogdanov <bogdoslavik@gmail.com>
2024-07-22 00:22:19 +0700

    deliverBath

--- contracts/Coprocessor.sol
+++ contracts/Coprocessor.sol
@@ -16,23 +16,57 @@ contract Coprocessor {
 
     event Bridge(uint indexed toChainId, address indexed receiver, uint indexed valueIn, uint coprocessorBalance);
     event Deliver(uint indexed fromChainId, address indexed receiver, uint indexed valueIn, uint coprocessorBalance);
+    event Failed(uint indexed fromChainId, address indexed receiver, uint indexed valueIn, uint coprocessorBalance);
     event Deposit(address indexed depositor, uint indexed valueIn, uint coprocessorBalance);
     event Redeem(address indexed receiver, uint indexed amount, uint coprocessorBalance);
 
     /// @dev Bridge the value to another chain
+    /// @param toChainId The chain id to bridge to
+    function bridge(uint toChainId)
+    external payable  {
+        coprocessor.transfer(msg.value);
+        emit Bridge(toChainId, msg.sender, msg.value, coprocessor.balance);
+    }
+
+/*    /// @dev Bridge the value to another chain
     /// @param toChainId The chain id to bridge to
     /// @param receiver The receiver address on the other chain
     function bridge(uint toChainId, address receiver)
     external payable  {
         coprocessor.transfer(msg.value);
         emit Bridge(toChainId, receiver, msg.value, coprocessor.balance);
-    }
+    }*/
 
     function deliver(uint fromChainId, address payable receiver)
     external payable  {
-        if (msg.sender != coprocessor) revert();
+        require(msg.sender == coprocessor);
         receiver.transfer(msg.value);
         emit Deliver(fromChainId, receiver, msg.value, coprocessor.balance);
+    }
+
+    function deliverBath(uint[] fromChainId, address payable[] receiver, uint[] value)
+    external payable  {
+        require(msg.sender == coprocessor);
+        require(fromChainId.length == receiver.length && receiver.length == value.length);
+        uint coprocessorBalance = coprocessor.balance;
+        uint valueProcessed = 0;
+
+        for (uint i = 0; i < fromChainId.length; i++) {
+            uint value = value[i];
+            if (receiver[i].send(value)) {
+                emit Deliver(fromChainId[i], receiver[i], value, coprocessorBalance);
+                valueProcessed += value;
+            } else {
+                emit Failed(fromChainId[i], receiver[i], value, coprocessorBalance);
+            }
+        }
+        // Send back the remaining value
+        if (valueProcessed < msg.value) {
+            uint change = msg.value - valueProcessed;
+            coprocessor.transfer(change);
+            valueProcessed += change;
+        }
+        require(valueProcessed == msg.value);
     }
 
 
@@ -58,7 +92,7 @@ contract Coprocessor {
     }
 
     function callback(string calldata _result, uint256 _job_id) public {
-        if (msg.sender != coprocessor) revert();
+        require(msg.sender == coprocessor);
         jobs[_job_id] = _result;
     }
 }
